e60fa6a06a0cd5109bd9c7e13b362a5bf731d33f
a-oboh <albertoboh7@gmail.com>
2026-08-17 21:07:35 +0100

    Refactor migration files and update database schema initialization

--- migrations/env.py
+++ migrations/env.py
@@ -4,8 +4,16 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from football_quant.db.base import Base
-from football_quant.db import models
+from football_analytics.db.base import Base
+from football_analytics.db import models
+
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+database_url = os.environ["DATABASE_URL"]
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -40,7 +48,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = database_url
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -60,15 +68,15 @@ def run_migrations_online() -> None:
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        {
+            "sqlalchemy.url": database_url,
+        },
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
