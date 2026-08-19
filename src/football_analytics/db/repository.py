4737ad829f07f8837021f93e0f65bc9ec27fe00e
a-oboh <albertoboh7@gmail.com>
2026-08-19 22:35:53 +0100

    Implement get_or_create_season method in FootballRepository

--- src/football_analytics/db/repository.py
+++ src/football_analytics/db/repository.py
@@ -1,6 +1,21 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+from .models.season import Season
 
 
 class FootballRepository:
     def __init__(self, session: Session):
         self.session = session
+
+    def get_or_create_season(self, name: str) -> Season:
+        stmt = select(Season).where(Season.name == name)
+
+        season = self.session.scalar(stmt)
+
+        if season is None:
+            season = Season(name=name)
+            self.session.add(season)
+            self.session.flush()
+
+        return season
